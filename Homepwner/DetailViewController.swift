5165466dd3e152a07c3fac58fb2dd5f15e60b4fe
Alex Louzao <alexlouzao@Alexs-MacBook-Pro.local>
2017-04-01 11:45:43 -0400

    ch 15 challenges done, need to test

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -12,24 +12,46 @@ import UIKit
 class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
     
     @IBOutlet var imageView: UIImageView!
+    
     @IBAction func takePicture(_ sender: UIBarButtonItem) {
         let imagePicker = UIImagePickerController()
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
             imagePicker.sourceType = .camera
+            
+            let overlayView = UIView(frame: imagePicker.cameraOverlayView!.frame)
+            
+            let crosshairLabel = UILabel()
+            crosshairLabel.text = "+"
+            crosshairLabel.font = UIFont.systemFont(ofSize: 50)
+            crosshairLabel.translatesAutoresizingMaskIntoConstraints = false
+            overlayView.addSubview(crosshairLabel)
+            
+            crosshairLabel.centerXAnchor.constraint(equalTo: overlayView.centerXAnchor).isActive = true
+            crosshairLabel.centerYAnchor.constraint(equalTo: overlayView.centerYAnchor).isActive = true
+            
+            overlayView.isUserInteractionEnabled = false
+            
+            imagePicker.cameraOverlayView = overlayView
         } else {
             imagePicker.sourceType = .photoLibrary
         }
+        
         imagePicker.delegate = self
+        imagePicker.allowsEditing = true
         
         present(imagePicker, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        //let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let image = info[UIImagePickerControllerEditedImage] as! UIImage
         imageStore.setImage(image, forKey: item.itemKey)
         imageView.image = image
         dismiss(animated: true, completion: nil)
+        removePictureBarButton.isEnabled = true
     }
+    
     
     var imageStore: ImageStore!
     
@@ -66,16 +88,16 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         
         nameField.text = item.name
         serialField.text = item.serialNumber
-        valueField.text =
-            numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
         dateLabel.text = dateFormatter.string(from: item.dateCreated)
         
         let key = item.itemKey
         let imageToDisplay = imageStore.image(forKey: key)
         imageView.image = imageToDisplay
+        removePictureBarButton.isEnabled = imageView.image != nil
         
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         view.endEditing(true)
@@ -88,7 +110,9 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         } else {
             item.valueInDollars = 0
         }
+
     }
+    
     
     func textFieldShouldReturn(_ textField: UITextField)->Bool{
         textField.resignFirstResponder()
@@ -110,4 +134,34 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         }
     }
     
+    var flexibleSpaceBarButton: UIBarButtonItem!
+    var removePictureBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        flexibleSpaceBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        removePictureBarButton = UIBarButtonItem(
+            barButtonSystemItem: .trash,
+            target: self, action: #selector(removePicture(_:)))
+        
+        // Locate the toolbar at bottom and add to it the barButtons
+        for subView in self.view.subviews {
+            if let toolbar = subView as? UIToolbar {
+                var toolbarItems = toolbar.items
+                toolbarItems?.append(flexibleSpaceBarButton)
+                toolbarItems?.append(removePictureBarButton)
+                toolbar.setItems(toolbarItems, animated: true)
+                break
+            }
+        }
+    }
+    
+    @IBAction func removePicture(_ sender: UIBarButtonItem) {
+        imageStore.deleteImage(forKey: item.itemKey)
+        imageView.image = nil
+        removePictureBarButton.isEnabled = false
+    }
+    
 }
+
